b4399416c10aa7757f585702c63e566f86de858f
scottzyang <scottzyang@gmail.com>
2024-04-15 17:42:49 -0500

    Add search bar filtering

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, FlatList, SafeAreaView, KeyboardAvoidingView, TextInput } from 'react-native';
 import { cats, dogs, petTypes } from './breeds';
 import Item from './Item';
+import { useState } from 'react';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState(null)
+
+  const searchTermUpdate = (text) => {
+    console.log("updating search term")
+    setSearchTerm(text)
+  }
+
+  const searchResults = (data, searchTerm) => {
+    console.log("Outputting search results")
+    if (!searchTerm) {
+      return data
+    }
+    // filter data based on search term
+    const searchResults = data.filter((item) => {
+      return item.breed.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+    if (searchResults.length > 0) {
+      return searchResults
+    }
+  }
+
+  console.log(searchResults(cats, "abyss"))
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
+      <KeyboardAvoidingView style={styles.listContainer}>
+      <TextInput style={styles.input} placeholder="Search Breeds, i.e American" onChangeText={text => searchTermUpdate(text)}></TextInput>
         <FlatList style={styles.flatListContent}
-          data={cats}
+          data={searchResults(cats, searchTerm)}
           renderItem={({ item, index }) => <Item data={item} index={index}/>}
           keyExtractor={(item, index) => index.toString()}
         />
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -23,11 +48,19 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   flatListContent: {
     width: '100%',
-  }
+  },
+  input: {
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 0, // Add top margin to adjust position
+  },
 });
+
--- TextInput.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TextInput(props) {
-  const { searchTerm, setSearchTerm } = props;
-
-  return {
-    // Implement your solution here
-  };
-}
